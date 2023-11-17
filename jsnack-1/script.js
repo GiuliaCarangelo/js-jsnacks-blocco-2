242e0b0eeb12ed3c3fbb433713e62b0e9e4f25da
GiuliaCarangelo <tnhkagura@gmail.com>
2023-11-17 11:27:57 +0100

    add while loop

---
+++ jsnack-1/script.js
@@ -0,0 +1,14 @@
+// Crea un array vuoto 
+// e chiedi all'utente un numero da inserire nell'array. Continua a chiedere i numeri all'utente e a inserirli nell'array
+// fino a quando la somma degli elementi è minore di 50.
+
+let array = [];
+let userInput = parseInt(prompt('inserisci un numero'));
+array.push(userInput);
+
+while (somma < 50){
+    userInput = parseInt(prompt('inserisci un numero'));
+    
+}
+
+
