72c0ce8ec1b07dbb471e9e7cc0e3c4b3e2884d67
GiuliaCarangelo <tnhkagura@gmail.com>
2023-11-17 12:02:53 +0100

    add lists and .concat let

---
+++ jsnack-2/script.js
@@ -0,0 +1,10 @@
+// Generatore di "nomi cognomi" casuali:
+//  il Grande Gatsby ha una lista di nomi 
+//  e una lista di cognomi,
+//   e da queste vuole generare una falsa lista di invitati con nome e cognome.
+let nameList = ['Jay', 'Daisy', 'Myrtle', 'Meyer', 'Nick', 'Jordan'];
+let lastNameList = ['Gatsby', 'Buchanan', 'Wilson', 'Wolfsheim', 'Carraway', 'Baker'];
+
+let nameGen = lastNameList.concat(nameList);
+console.log(nameGen);
+
