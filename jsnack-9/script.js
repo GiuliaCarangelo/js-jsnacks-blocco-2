56331bc3f38e9ceea0753fb771ca9a27cdcc125b
GiuliaCarangelo <tnhkagura@gmail.com>
2023-11-24 12:34:31 +0100

    add array

--- jsnack-9/script.js
+++ jsnack-9/script.js
@@ -1,24 +1,25 @@
 // Crea 10 oggetti che rappresentano una zucchina, indicandone per ognuno varietà, peso e lunghezza. Calcola quanto pesano tutte le zucchine.
 
-let zucchinis = [
+const zucchinis = [
 
-const zucchiniSmall = {
+zucchiniSmall = {
     variety: 'green',
     weight: 3,
     length: 8
 };
 
-const zucchiniMedium = {
+zucchiniMedium = {
     variety: 'yellow',
     weight: 4,
     length: 7
 };
 
-const zucchiniLarge = {
+zucchiniLarge = {
     variety: 'green',
     weight: 5,
     length: 10
 };
+
 ]
 
 let totalWeight = zucchinis
